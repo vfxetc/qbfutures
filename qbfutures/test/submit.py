cf1d101ed9b83046479888949977a0474bb2a3d3
Mike Boers <mboers@keypics.com>
2012-10-22 13:19:10 -0700

    Cleaned up examples

--- qbfutures/test/submit.py
+++ qbfutures/test/submit.py
@@ -4,10 +4,31 @@ import os
 from concurrent.futures import as_completed
 
 from ..core import Executor
-from .work import func
 
-import qb
 
+
+## SINGLE JOB
+
+print 'SUBMIT'
+
+future = Executor().submit('qbfutures.test.work:func', 'single')
+print future
+print future.result()
+print
+
+
+## MAP
+
+print 'MAP'
+
+for x in Executor().map('qbfutures.test.work:func', xrange(5)):
+    print x
+
+print
+
+## BATCH
+
+print 'BATCH'
 
 with Executor().batch('Test Batch') as batch:
     first = batch.submit_ext('qbfutures.test.work:func', ['first'], name='first')
@@ -15,21 +36,7 @@ with Executor().batch('Test Batch') as batch:
     time_f = batch.submit(time.time)
     cwd = batch.submit(os.getcwd)
 
-print 'as_completed'
 for future in as_completed(batch.futures):
     print future.job_id, future.agenda_id, future.result()
-
-exit()
-
-future = Executor().submit_ext('qbfutures.test.work:maya_test', maya=2011, name='QBFutures Maya Test')
-print future
-print future.result()
+    
 print
-
-future = Executor().submit('qbfutures.test.work:func', 'single')
-print future
-print future.result()
-print
-
-for x in Executor().map('qbfutures.test.work:func', xrange(40)):
-    print x
