ba34f6e060592cb46f23f01ece6b64d582335e82
Mike Boers <westernx@mikeboers.com>
2015-06-23 12:52:02 -0700

    move the templates into the package (so they will be installed)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qbfutures',
-    version='0.1-dev',
+    version='0.2.0',
     description='`concurrent.futures` for Qube.',
     url='http://github.com/westernx/qbfutures',
     
@@ -12,6 +12,8 @@ setup(
     author_email='qbfutures@mikeboers.com',
     license='BSD-3',
     
+    install_package_data=True,
+
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
